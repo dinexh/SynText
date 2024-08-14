ef3c8499b5eaabca356d60b6f33dcab8c3a77a6f
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-14 13:41:20 +0530

    making changes to footer

--- app/src/components/footer/footer.jsx
+++ app/src/components/footer/footer.jsx
@@ -3,7 +3,12 @@ const Footer = () => {
     return ( 
         <footer>
             <div className="footer-in">
-                <p>Designed and Developer by <a href="https://dineshkorukonda.in">Dinesh Korukonda</a> | All Rights Reservesd</p>
+                <div className="footer-in-main">
+                    
+                </div>
+                <div className="footer-in-lower">
+                    <p>Designed and Developer by <a href="https://dineshkorukonda.in">Dinesh Korukonda</a> | All Rights Reservesd</p>
+                </div>
             </div>
         </footer>
      );
