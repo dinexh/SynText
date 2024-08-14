bf1c02cdc89b4d9cf14c0666e3af8946113cb6e1
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-14 16:04:05 +0530

    landing page home

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
-import Nav from './components/nav/nav';
-import Footer from './components/footer/footer';
+import Home from './pages/home/home'
 function App() {
   return (
-    <div className="App">
-        <div className="App-header">
-        <Nav /> 
-        </div>
-        <div className="App-footer">
-          <Footer/>
+    <div className="App"> 
+        <div className="App-home">
+          <Home/>
+        
         </div>
     </div>
   );
---
+++ app/src/pages/home/home.jsx
@@ -0,0 +1,28 @@
+import HomeImage from "../../Assets/home.svg"
+import './home.css'
+const Home = () => {
+    return (  
+        <div className="home-container">
+            <div className="home-container-in">
+                <div className="home-container-in-one">
+                    <img src={HomeImage} className="HomeImage" alt="home image" />
+                </div>
+                <div className="home-container-in-two">
+                    <h1>SynText</h1>
+                    <h2>Connect Instantly, Chat Effortlessly.</h2>
+                    <p>Transform your messaging experience with SynText—where every chat is effortless, every connection is instant, and every conversation feels personal. Discover seamless communication that keeps you close to those who matter most, no matter where you are.</p>
+                    <button>
+                        Join Now!
+                    </button>
+                </div>
+            </div>
+            <div className="home-footer">
+                <div className="home-footer-in">
+                    <p>Designed and Developed by <a href="#">Dinesh Korukonda</a> | All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home
