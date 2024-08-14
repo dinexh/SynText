e3ec29686d421db48c261d3c2340d50a0139e01e
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-14 18:59:42 +0530

    started auth page

--- app/src/pages/auth/auth.jsx
+++ app/src/pages/auth/auth.jsx
@@ -1,17 +1,20 @@
 import './auth.css';
-
+import React from 'react';
+import authImage from "../../Assets/auth-gif-2.gif";
 const Auth = () => {
   return (
-    <div className="auth-container">
-      <div className="auth-container-in">
-        <div className="auth-container-in-one">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus fugiat id et velit perspiciatis incidunt ipsa, quos pariatur quae earum ducimus eos aut sunt nam sapiente? Voluptatibus, impedit voluptates.
+        <div className="auth-container">
+            <div className="auth-container-in">
+                    <div className="auth-container-in-one">
+                    
+                    </div>
+                    <div className="auth-container-in-two">
+                        <div className="auth-container-two-image">
+                            <img src={authImage} className='AuthImage' alt="" />
+                        </div>
+                    </div>
+            </div>
         </div>
-        <div className="auth-container-in-two">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro maxime quia ex dolorum velit sint suscipit est autem doloremque eum? Rem nesciunt saepe dolorem rerum culpa possimus aut, perferendis maxime?
-        </div>
-      </div>
-    </div>
   );
 }
 
